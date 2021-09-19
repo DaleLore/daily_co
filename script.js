e985121058b76c447232e0aec4d513fe18d12bf1
DaleLore <DaleLore@protonmail.com>
2021-09-19 12:24:02 -0400

    sendAppMessage to alert participants

--- script.js
+++ script.js
@@ -1,37 +1,105 @@
-// Local_data stores the session_id and hand state of the local user.
-      let local_data = {
-          session_id: "",
-          handState: false,
-      }
+let localParticipant = {
+  handState: {},
+  session_id: "",
+  username: ""
+}
+
+callFrame = window.DailyIframe.createFrame(document.getElementById("iframe"), {
+  showLeaveButton: true
+});
 
 async function run() {
   let room = { url: "https://dalelore.daily.co/Raise-your-hand" };
 
-    callFrame = window.DailyIframe.createFrame(document.getElementById("iframe"), {
-    showLeaveButton: true
-  });
-
   await callFrame.join({ url: room.url });
-  
+  callFrame
+  .on("participant-joined", updateParticipantList)
+  .on("left", leftCall)
+  .on("app-message", updateHandState);
 }
 
-function raiseYourHand() {
-    console.log('Hello');
-    var x = document.getElementById("toggleHand");
+async function updateParticipantList(e){
+  console.log("updated list here")
+  participants = callFrame.participants();
+  console.log(participants)
+  let wrapper = document.getElementById("participant_list");
+  wrapper.innerHTML = "";
+  console.log(e)
+}
+
+function leftCall(){
+  console.log("bye felicia")
+}
+
+function updateHandState(){
+  console.log("raised hands")
+}
+
+
+function raiseYourHand(){
+  console.log("hand raised")
+  
+  let updateLocalParticipant = {
+    session_id: callFrame.participants().local.session_id,
+    username: callFrame.participants().local.user_name
+  }
+  console.log(updateLocalParticipant)
+  
+  var x = document.getElementById("toggleHand");
     if (x.innerHTML === "Need to ask a question?") {
+    console.log('Hello');
       x.innerHTML = "Your Hand is Raised!";
+      raisedHand = true
+      console.log(raisedHand)
     } else {
       x.innerHTML = "Need to ask a question?";
+      raisedHand = false
+      console.log(raisedHand)
     }
-    var y = document.getElementById("participant_list");
+
+    var y = document.getElementById("raisedHands_list");
     let username = callFrame.participants().local.user_name
     
     if (x.innerHTML === "Your Hand is Raised!") {
         y.innerHTML = username;
+        callFrame.sendAppMessage(username + "raised their hand");
       } else {
         y.innerHTML = " ";
       }
-  }
+}
+
+
+
+// function raiseYourHand() {
+//   let username = callFrame.participants().local.user_name
+
+//     var x = document.getElementById("toggleHand");
+//     if (x.innerHTML === "Need to ask a question?") {
+//     console.log('Hello');
+//       x.innerHTML = "Your Hand is Raised!";
+//       raisedHand = true
+//       console.log(raisedHand)
+//       console.log(username)
+
+//       update = {
+//         status: raisedHand,
+//         participant: username
+//       }
+//       message = callFrame.sendAppMessage(update, "*");      
+//     } else {
+//       x.innerHTML = "Need to ask a question?";
+//       raisedHand = false
+//       console.log("in else", raisedHand)
+//       update = {
+//         status: raisedHand,
+//         participant: username
+//       }
+//       message = callFrame.sendAppMessage(update, "*"); 
+//     }
+//     // console.log(alert())
+//   }
+
+  
 
 // JS AddEventListeners -> replaced with
 // document.addEventListener('DOMContentLoaded', init, false);
